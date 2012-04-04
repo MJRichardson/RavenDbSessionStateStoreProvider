91c692296dc86b3d0aa961cc6a1d3f8f99bff769
MJ Richardson <michael.james.richardson@gmail.com>
2012-04-04 12:58:33 +1000

    Added sample application

---
+++ sample/RavenDbSessionStateSample/Controllers/RavenDbSessionStateController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace RavenDbSessionStateSample.Controllers
+{
+    public class RavenDbSessionStateController : Controller
+    {
+        private const string SessionStateKey = "__key";
+
+         public ViewResult Index()
+         {
+             ViewBag.Value = Session[SessionStateKey];
+
+             return View();
+         }
+
+        public ActionResult SetValue( string value)
+        {
+            Session[SessionStateKey] = value;
+
+            return RedirectToAction("Index");
+        }
+    }
+}
